ca2a7d38af97a612ace254259e72869984271cb4
Fernando Rebelo <fernandorebelo.dev@gmail.com>
2024-07-27 08:48:40 -0300

    first commit

---
+++ src/main/java/com/fernando/learn_spring_framework/AppGamingBasicJava.java
@@ -0,0 +1,21 @@
+package com.fernando.learn_spring_framework;
+
+import com.fernando.learn_spring_framework.game.GameRunner;
+import com.fernando.learn_spring_framework.game.MarioGame;
+import com.fernando.learn_spring_framework.game.PacManGame;
+import com.fernando.learn_spring_framework.game.SuperContraGame;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		
+//		var game = new MarioGame();
+//		var game = new SuperContraGame();
+		var game = new PacManGame();
+		
+		var gameRunner = new GameRunner(game);
+		
+		gameRunner.run();
+	}
+
+}
---
+++ src/main/java/com/fernando/learn_spring_framework/game/GameConsole.java
@@ -0,0 +1,9 @@
+package com.fernando.learn_spring_framework.game;
+
+public interface GameConsole {
+	public void up();
+	public void down();
+	public void left();
+	public void right();
+
+}
